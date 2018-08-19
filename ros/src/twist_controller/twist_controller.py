ccbe6f7eb7dc5f05d675e3d6dfbcb701f4eb0418
student <nalini.sharma@gmail.com>
2018-08-19 09:37:21 -0700

    Added more twist controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,22 +1,56 @@
 
+
+from pid import PID
+from yaw_controller import YawController
+from lowpass import LowPassFilter
+
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
-from pid import pid
-from yaw_controller import yaw_controller
-
-# Nalini - added 8/18/2018
+# Nalini - added 8/19/2018
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        self.wheel_base = kwargs[wheel_base]
+	def __init__(self, *args, **kwargs):
+		# TODO: Implement
+		self.wheel_base = kwargs['wheel_base']
+		self.steer_ratio = kwargs['steer_ratio']
+		self.min_speed = 0
+		self.max_lat_accel = kwargs['max_lat_accel']
+		self.max_steer_angle = kwargs['max_steer_angle']
+		self.decel_limit = kwargs['decel_limit']
+		self.accel_limit = kwargs['accel_limit']
+		self.wheel_radius = kwargs['wheel_radius']
+
+		Kp = 1
+		Ki = 0
+		Kd = 0.05
+
+		self.pid_controller = PID(Kp, Ki, Kd, mn = self.decel_limit, mx = self.accel_limit)
+
+		self.yaw_controller = Yaw_Controller(self.wheel_base, self.steer_ratio, self.min_speed, self.max_lat_accel, self.max_steer_angle)
+
+		tau = 0.5
+		ts = .02
+
+		self.vel_lpf = LowPassFilter(tau, ts)
+
+		self.vehicle_mass = kwargs['vehicle_mass'] + kwargs['fuel_capacity'] * GAS_DENSITY
+		self.brake_deadband = kwargs['brake_deadband']
 
 
-        pass
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+		
+
+	def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+		# TODO: Change the arg, kwarg list to suit your needs
+		# Return throttle, brake, steer
+		if not dbw_enabled:
+			self.pid_controller.reset()
+			return 0., 0., 0.
+
+
+		current_vel = self.vel_lpf.filt(current_vel)
+
+
+		return 1., 0., 0.
